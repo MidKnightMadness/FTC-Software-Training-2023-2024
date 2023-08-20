eb4546921816b1486d3accf9a90719342fd2d158
Tysty <tylerstyle05@gmail.com>
2023-08-20 16:19:03 -0700

    Added first 3 lessons

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Challenges/Challenge1.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Challenges;
+
+public class Challenge1 {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lessons/OpModeLesson.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lessons/OpModeLesson.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.Lessons;
 
-public class OpModeLesson {
+//importing libraries (will usually add it automatically)
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp //This determines what kind of program you are using (TeleOp or Auto). TeleOp is better for experimentation because Auto ends the program after 30 seconds
+public class OpModeLesson extends OpMode { //Make sure to put "extends OpMode"
+    //There are 2 phases in an FTC program. Initialize and Run Mode
+
+    @Override //Honestly idk exactly what it does, but its kinda important to make sure to have it before a method
+    public void init(){ // This function is what happens during initialization. Used for setting up sensors, motors, etc
+
+    }
+    @Override
+    public void start(){ // This function is what happens at the very start of run mode
+
+    }
+    @Override
+    public void loop(){ // This function runs constantly during run mode. Will run forever until program is stopped
+
+    }
 }
